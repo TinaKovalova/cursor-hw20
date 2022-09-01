ec344dffeffec995200b21b493816d5c575cf802
Tina Kovalova <kovalevatkv@gmail.com>
2022-09-01 22:16:53 +0300

    changed baseURL for actions

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -1,7 +1,7 @@
 import {ADD_USER, GET_USERS} from "./types";
 import axios from "axios";
 
-axios.defaults.baseURL = 'http://localhost:3000';
+axios.defaults.baseURL = 'https://server-hw-cursor.herokuapp.com/';
 
 
 export const getUsersAction = () => {
